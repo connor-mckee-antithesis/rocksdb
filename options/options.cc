2aa76a35ea98108e97c16dea600c81e3bb538d89
Connor McKee <connor.mckee@antithesis.com>
2025-02-13 17:34:00 -0500

    change # background threads

--- options/options.cc
+++ options/options.cc
@@ -696,8 +696,8 @@ ColumnFamilyOptions* ColumnFamilyOptions::OptimizeUniversalStyleCompaction(
 
 DBOptions* DBOptions::IncreaseParallelism(int total_threads) {
   max_background_jobs = total_threads;
-  env->SetBackgroundThreads(total_threads, Env::LOW);
-  env->SetBackgroundThreads(total_threads, Env::HIGH);
+  env->SetBackgroundThreads(2, Env::LOW);
+  env->SetBackgroundThreads(2, Env::HIGH);
   return this;
 }
 
